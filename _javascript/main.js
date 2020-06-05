732b387667cb641ab25eafeccb1021cfa7ba5cb1
Rafał Konofalski <rafal.konofalski@gmail.com>
2020-06-05 18:22:15 +0200

    desktop completed

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+    const accLabel = document.querySelectorAll('.accordion__label');
+    const accContent = document.querySelectorAll('.accordion__content');
+
+    [...accLabel].forEach(element => {
+        element.addEventListener('click', function () {
+            [...accLabel].forEach(item => {
+                item.classList.remove('active');
+            });
+
+            [...accContent].forEach(item => {
+                item.style.maxHeight = null;
+            });
+
+            this.classList.add('active');
+            let panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + 'px';
+            }
+        });
+    });
 });
